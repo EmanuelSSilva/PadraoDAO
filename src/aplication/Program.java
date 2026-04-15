64c17976710a6e98319e45364329b05fa6a40611
Emanuel Silva <emanuellinssilva65@gmail.com>
2026-04-15 15:56:30 -0300

    Calsee Departamento

---
+++ src/aplication/Program.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import model.entites.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "caderno");
+		System.out.println(obj);
+	}
+
+}
