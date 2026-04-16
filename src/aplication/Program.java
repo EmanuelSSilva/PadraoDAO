2f6f4a0244012cd16cdb1173a9b17eb6fd489802
Emanuel Silva <emanuellinssilva65@gmail.com>
2026-04-16 14:53:23 -0300

    Implemento o Insert no Banco

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,6 @@
 package aplication;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -11,23 +12,29 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		VendedorDao vendedor = DaoFactory.createVendedorDao();
+		VendedorDao vendedor = DaoFactory.createVendedorDao();//criando o vendedor
+		
 		System.out.println("=== TESTE 1: Vendedor findById ===");// teste do vendendo, buscando informaçoes no banco
 		Vendedor vend = vendedor.findById(3);
 		System.out.println(vend);
 		
-		System.out.println("\n=== TESTE 2: Vendedor findByDepartamento ===");// teste do vendendo, buscando informaçoes no banco
+		System.out.println("\n=== TESTE 2: Vendedor findByDepartamento ===");// buscando informaçoes no banco por departamento
 		Departamento departamento = new Departamento(2, null);
 		List<Vendedor> list = vendedor.findByDepartamento(departamento);
 		for (Vendedor obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TESTE 3: Vendedor findAll ===");// teste do vendendo, buscando informaçoes no banco
+		System.out.println("\n=== TESTE 3: Vendedor findAll ===");// teste de busca de todos os vendedores
 		list = vendedor.findAll();
 		for (Vendedor obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 4: Vendedor Insert ===");// Inserindo vendedor no banco
+		Vendedor novoVendedor = new Vendedor(null, "Antonio", "antonio@gmail.com", new Date(), 4000.0, departamento );
+		vendedor.insert(novoVendedor);
+		System.out.println("Inserido! Novo Id - " + novoVendedor.getId());
 		
 	}
 
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,9 +1,11 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,28 +19,56 @@ import model.entites.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao {
 
-	private Connection conn;// Realizando injeção 
-	
+	private Connection conn;// Realizando injeção
+
 	public VendedorDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Vendedor obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ " VALUES " + " (?, ?, ?, ?, ?) ", Statement.RETURN_GENERATED_KEYS);// retornando o ID do vendedor
+																							// inserido.
+
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getDataNascimento().getTime()));
+			st.setDouble(4, obj.getBaseSalario());
+			st.setInt(5, obj.getDepartamento().getId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("ERROR! Nenhuma linha foi alterada.");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+
+		}
 	}
 
 	@Override
 	public void update(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -47,24 +77,20 @@ public class VendedorDaoJDBC implements VendedorDao {
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-				"SELECT seller.*, department.Name as DepName "
-				+ "FROM seller INNER JOIN department "
-				+ "ON seller.DepartmentId = department.Id "
-				+ "WHERE seller.Id = ?");
+					"SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 			st.setInt(1, id);
-			rs =st.executeQuery();
+			rs = st.executeQuery();
 			if (rs.next()) {
 				Departamento dep = instanciaDepatarmento(rs);
 				Vendedor vend = instaciaVendedor(rs, dep);
 				return vend;
-			}	
-			return null;	
-			
-		}
-		catch(SQLException e) {
+			}
+			return null;
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
@@ -88,88 +114,74 @@ public class VendedorDaoJDBC implements VendedorDao {
 		return dep;
 	}
 
-	
-	
 	@Override
 	public List<Vendedor> findAll() {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-				"SELECT seller.*, department.Name as DepName "
-				+ "FROM seller INNER JOIN department "
-				+ "ON seller.DepartmentId = department.Id "
-				+ "ORDER BY Name ");
-						
+					"SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name ");
+
 			rs = st.executeQuery();
-			
-			List <Vendedor> list = new ArrayList<>();
-			Map<Integer, Departamento> map= new HashMap<>();
-			
+
+			List<Vendedor> list = new ArrayList<>();
+			Map<Integer, Departamento> map = new HashMap<>();
+
 			while (rs.next()) {
 				Departamento dep = map.get(rs.getInt("DepartmentId"));
-				if(dep == null) {
+				if (dep == null) {
 					dep = instanciaDepatarmento(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
-				
+
 				Vendedor vend = instaciaVendedor(rs, dep);
 				list.add(vend);
-			}	
-			return list;	
-			
-		}
-		catch(SQLException e) {
+			}
+			return list;
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
 	}
 
-	
-	
 	@Override
 	public List<Vendedor> findByDepartamento(Departamento departamento) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-				"SELECT seller.*, department.Name as DepName "
-				+ "FROM seller INNER JOIN department "
-				+ "ON seller.DepartmentId = department.Id "
-				+ "WHERE DepartmentId = ? "
-				+ "ORDER BY Name ");
+					"SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name ");
 			st.setInt(1, departamento.getId());
-			
+
 			rs = st.executeQuery();
-			
-			List <Vendedor> list = new ArrayList<>();
-			Map<Integer, Departamento> map= new HashMap<>();
-			
+
+			List<Vendedor> list = new ArrayList<>();
+			Map<Integer, Departamento> map = new HashMap<>();
+
 			while (rs.next()) {
 				Departamento dep = map.get(rs.getInt("DepartmentId"));
-				if(dep == null) {
+				if (dep == null) {
 					dep = instanciaDepatarmento(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
-				
+
 				Vendedor vend = instaciaVendedor(rs, dep);
 				list.add(vend);
-			}	
-			return list;	
-			
-		}
-		catch(SQLException e) {
+			}
+			return list;
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-		
+
 	}
-	
 
 }
